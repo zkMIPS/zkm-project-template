82fd91c4178299e9ee830c627fa73f8089481881
xander42280 <xander.z@zkm.io>
2024-09-08 12:59:44 +0800

    cargo fmt

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["src/proto/stage.proto"], &["src/proto"])?;
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["src/proto/stage.proto"], &["src/proto"])?;
     Ok(())
 }
