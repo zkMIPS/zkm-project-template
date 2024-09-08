719c05ec4723ce93e6ee34728767fa235c9fda88
xander.z <162873981+xander42280@users.noreply.github.com>
2024-09-08 15:27:35 +0800

    sdk prover network (#6)
    * sdk(network prover)
    
    * fmt
    
    * default
    
    * use async new
    
    * Optimize Protocol
    
    * Option proto3_optional
    
    * cargo fmt

---
+++ sdk/src/lib.rs
@@ -0,0 +1,42 @@
+pub mod local;
+pub mod network;
+pub mod prover;
+
+use network::prover::NetworkProver;
+use prover::Prover;
+use std::env;
+
+pub struct ProverClient {
+    pub prover: Box<dyn Prover>,
+}
+
+impl ProverClient {
+    pub async fn new() -> Self {
+        #[allow(unreachable_code)]
+        match env::var("ZKM_PROVER")
+            .unwrap_or("network".to_string())
+            .to_lowercase()
+            .as_str()
+        {
+            // "local" => Self {
+            //     prover: Box::new(CpuProver::new()),
+            // },
+            "network" => Self {
+                prover: Box::new(NetworkProver::new().await.unwrap()),
+            },
+            _ => panic!(
+                "invalid value for ZKM_PROVER enviroment variable: expected 'local', or 'network'"
+            ),
+        }
+    }
+
+    // pub fn local() -> Self {
+    //     Self { prover: Box::new(CpuProver::new()) }
+    // }
+
+    pub async fn network() -> Self {
+        Self {
+            prover: Box::new(NetworkProver::new().await.unwrap()),
+        }
+    }
+}
---
+++ sdk/src/local/cpu.rs
@@ -0,0 +1,455 @@
+// use serde::{Deserialize, Serialize};
+// use std::env;
+// use std::fs::File;
+// use std::io::prelude::*;
+// use std::io::BufReader;
+// use std::ops::Range;
+// use std::time::Duration;
+
+// use plonky2::field::goldilocks_field::GoldilocksField;
+// use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+// use plonky2::util::timing::TimingTree;
+// use plonky2x::backend::circuit::Groth16WrapperParameters;
+// use plonky2x::backend::wrapper::wrap::WrappedCircuit;
+// use plonky2x::frontend::builder::CircuitBuilder as WrapperBuilder;
+// use plonky2x::prelude::DefaultParameters;
+// use zkm_emulator::utils::{
+//     get_block_path, load_elf_with_patch, split_prog_into_segs, SEGMENT_STEPS,
+// };
+// use zkm_prover::all_stark::AllStark;
+// use zkm_prover::config::StarkConfig;
+// use zkm_prover::cpu::kernel::assembler::segment_kernel;
+// use zkm_prover::fixed_recursive_verifier::AllRecursiveCircuits;
+// use zkm_prover::proof;
+// use zkm_prover::proof::PublicValues;
+// use zkm_prover::prover::prove;
+// use zkm_prover::verifier::verify_proof;
+
+// const DEGREE_BITS_RANGE: [Range<usize>; 6] = [10..21, 12..22, 12..21, 8..21, 6..21, 13..23];
+
+// fn split_segments() {
+//     // 1. split ELF into segs
+//     let basedir = env::var("BASEDIR").unwrap_or("/tmp/cannon".to_string());
+//     let elf_path = env::var("ELF_PATH").expect("ELF file is missing");
+//     let block_no = env::var("BLOCK_NO").unwrap_or("".to_string());
+//     let seg_path = env::var("SEG_OUTPUT").expect("Segment output path is missing");
+//     let seg_size = env::var("SEG_SIZE").unwrap_or(format!("{SEGMENT_STEPS}"));
+//     let seg_size = seg_size.parse::<_>().unwrap_or(SEGMENT_STEPS);
+//     let args = env::var("ARGS").unwrap_or("".to_string());
+//     let args = args.split_whitespace().collect();
+
+//     let mut state = load_elf_with_patch(&elf_path, args);
+//     let block_path = get_block_path(&basedir, &block_no, "");
+//     if !block_no.is_empty() {
+//         state.load_input(&block_path);
+//     }
+//     let _ = split_prog_into_segs(state, &seg_path, &block_path, seg_size);
+// }
+
+// fn prove_single_seg_common(
+//     seg_file: &str,
+//     basedir: &str,
+//     block: &str,
+//     file: &str,
+//     seg_size: usize,
+// ) {
+//     let seg_reader = BufReader::new(File::open(seg_file).unwrap());
+//     let kernel = segment_kernel(basedir, block, file, seg_reader, seg_size);
+
+//     const D: usize = 2;
+//     type C = PoseidonGoldilocksConfig;
+//     type F = <C as GenericConfig<D>>::F;
+
+//     let allstark: AllStark<F, D> = AllStark::default();
+//     let config = StarkConfig::standard_fast_config();
+//     let mut timing = TimingTree::new("prove", log::Level::Info);
+//     let allproof: proof::AllProof<GoldilocksField, C, D> =
+//         prove(&allstark, &kernel, &config, &mut timing).unwrap();
+//     let mut count_bytes = 0;
+//     for (row, proof) in allproof.stark_proofs.clone().iter().enumerate() {
+//         let proof_str = serde_json::to_string(&proof.proof).unwrap();
+//         log::info!("row:{} proof bytes:{}", row, proof_str.len());
+//         count_bytes += proof_str.len();
+//     }
+//     timing.filter(Duration::from_millis(100)).print();
+//     log::info!("total proof bytes:{}KB", count_bytes / 1024);
+//     verify_proof(&allstark, allproof, &config).unwrap();
+//     log::info!("Prove done");
+// }
+
+// fn prove_multi_seg_common(
+//     seg_dir: &str,
+//     basedir: &str,
+//     block: &str,
+//     file: &str,
+//     seg_size: usize,
+//     seg_file_number: usize,
+//     seg_start_id: usize,
+// ) -> anyhow::Result<()> {
+//     type InnerParameters = DefaultParameters;
+//     type OuterParameters = Groth16WrapperParameters;
+
+//     type F = GoldilocksField;
+//     const D: usize = 2;
+//     type C = PoseidonGoldilocksConfig;
+
+//     if seg_file_number < 2 {
+//         panic!("seg file number must >= 2\n");
+//     }
+
+//     let total_timing = TimingTree::new("prove total time", log::Level::Info);
+//     let all_stark = AllStark::<F, D>::default();
+//     let config = StarkConfig::standard_fast_config();
+//     // Preprocess all circuits.
+//     let all_circuits =
+//         AllRecursiveCircuits::<F, C, D>::new(&all_stark, &DEGREE_BITS_RANGE, &config);
+
+//     let seg_file = format!("{}/{}", seg_dir, seg_start_id);
+//     log::info!("Process segment {}", seg_file);
+//     let seg_reader = BufReader::new(File::open(seg_file)?);
+//     let input_first = segment_kernel(basedir, block, file, seg_reader, seg_size);
+//     let mut timing = TimingTree::new("prove root first", log::Level::Info);
+//     let (mut agg_proof, mut updated_agg_public_values) =
+//         all_circuits.prove_root(&all_stark, &input_first, &config, &mut timing)?;
+
+//     timing.filter(Duration::from_millis(100)).print();
+//     all_circuits.verify_root(agg_proof.clone())?;
+
+//     let mut base_seg = seg_start_id + 1;
+//     let mut seg_num = seg_file_number - 1;
+//     let mut is_agg = false;
+
+//     if seg_file_number % 2 == 0 {
+//         let seg_file = format!("{}/{}", seg_dir, seg_start_id + 1);
+//         log::info!("Process segment {}", seg_file);
+//         let seg_reader = BufReader::new(File::open(seg_file)?);
+//         let input = segment_kernel(basedir, block, file, seg_reader, seg_size);
+//         timing = TimingTree::new("prove root second", log::Level::Info);
+//         let (root_proof, public_values) =
+//             all_circuits.prove_root(&all_stark, &input, &config, &mut timing)?;
+//         timing.filter(Duration::from_millis(100)).print();
+
+//         all_circuits.verify_root(root_proof.clone())?;
+
+//         // Update public values for the aggregation.
+//         let agg_public_values = PublicValues {
+//             roots_before: updated_agg_public_values.roots_before,
+//             roots_after: public_values.roots_after,
+//             userdata: public_values.userdata,
+//         };
+//         timing = TimingTree::new("prove aggression", log::Level::Info);
+//         // We can duplicate the proofs here because the state hasn't mutated.
+//         (agg_proof, updated_agg_public_values) = all_circuits.prove_aggregation(
+//             false,
+//             &agg_proof,
+//             false,
+//             &root_proof,
+//             agg_public_values.clone(),
+//         )?;
+//         timing.filter(Duration::from_millis(100)).print();
+//         all_circuits.verify_aggregation(&agg_proof)?;
+
+//         is_agg = true;
+//         base_seg = seg_start_id + 2;
+//         seg_num -= 1;
+//     }
+
+//     for i in 0..seg_num / 2 {
+//         let seg_file = format!("{}/{}", seg_dir, base_seg + (i << 1));
+//         log::info!("Process segment {}", seg_file);
+//         let seg_reader = BufReader::new(File::open(&seg_file)?);
+//         let input_first = segment_kernel(basedir, block, file, seg_reader, seg_size);
+//         let mut timing = TimingTree::new("prove root first", log::Level::Info);
+//         let (root_proof_first, first_public_values) =
+//             all_circuits.prove_root(&all_stark, &input_first, &config, &mut timing)?;
+
+//         timing.filter(Duration::from_millis(100)).print();
+//         all_circuits.verify_root(root_proof_first.clone())?;
+
+//         let seg_file = format!("{}/{}", seg_dir, base_seg + (i << 1) + 1);
+//         log::info!("Process segment {}", seg_file);
+//         let seg_reader = BufReader::new(File::open(&seg_file)?);
+//         let input = segment_kernel(basedir, block, file, seg_reader, seg_size);
+//         let mut timing = TimingTree::new("prove root second", log::Level::Info);
+//         let (root_proof, public_values) =
+//             all_circuits.prove_root(&all_stark, &input, &config, &mut timing)?;
+//         timing.filter(Duration::from_millis(100)).print();
+
+//         all_circuits.verify_root(root_proof.clone())?;
+
+//         // Update public values for the aggregation.
+//         let new_agg_public_values = PublicValues {
+//             roots_before: first_public_values.roots_before,
+//             roots_after: public_values.roots_after,
+//             userdata: public_values.userdata,
+//         };
+//         timing = TimingTree::new("prove aggression", log::Level::Info);
+//         // We can duplicate the proofs here because the state hasn't mutated.
+//         let (new_agg_proof, new_updated_agg_public_values) = all_circuits.prove_aggregation(
+//             false,
+//             &root_proof_first,
+//             false,
+//             &root_proof,
+//             new_agg_public_values,
+//         )?;
+//         timing.filter(Duration::from_millis(100)).print();
+//         all_circuits.verify_aggregation(&new_agg_proof)?;
+
+//         // Update public values for the nested aggregation.
+//         let agg_public_values = PublicValues {
+//             roots_before: updated_agg_public_values.roots_before,
+//             roots_after: new_updated_agg_public_values.roots_after,
+//             userdata: new_updated_agg_public_values.userdata,
+//         };
+//         timing = TimingTree::new("prove nested aggression", log::Level::Info);
+
+//         // We can duplicate the proofs here because the state hasn't mutated.
+//         (agg_proof, updated_agg_public_values) = all_circuits.prove_aggregation(
+//             is_agg,
+//             &agg_proof,
+//             true,
+//             &new_agg_proof,
+//             agg_public_values.clone(),
+//         )?;
+//         is_agg = true;
+//         timing.filter(Duration::from_millis(100)).print();
+
+//         all_circuits.verify_aggregation(&agg_proof)?;
+//     }
+
+//     let (block_proof, _block_public_values) =
+//         all_circuits.prove_block(None, &agg_proof, updated_agg_public_values)?;
+
+//     log::info!(
+//         "proof size: {:?}",
+//         serde_json::to_string(&block_proof.proof).unwrap().len()
+//     );
+//     let result = all_circuits.verify_block(&block_proof);
+
+//     let build_path = "../verifier/data".to_string();
+//     let path = format!("{}/test_circuit/", build_path);
+//     let builder = WrapperBuilder::<DefaultParameters, 2>::new();
+//     let mut circuit = builder.build();
+//     circuit.set_data(all_circuits.block.circuit);
+//     let mut bit_size = vec![32usize; 16];
+//     bit_size.extend(vec![8; 32]);
+//     bit_size.extend(vec![64; 68]);
+//     let wrapped_circuit = WrappedCircuit::<InnerParameters, OuterParameters, D>::build(
+//         circuit,
+//         Some((vec![], bit_size)),
+//     );
+//     log::info!("build finish");
+
+//     let wrapped_proof = wrapped_circuit.prove(&block_proof).unwrap();
+//     wrapped_proof.save(path).unwrap();
+
+//     total_timing.filter(Duration::from_millis(100)).print();
+//     result
+// }
+
+// fn prove_sha2_bench() {
+//     // 1. split ELF into segs
+//     let elf_path = env::var("ELF_PATH").expect("ELF file is missing");
+//     let seg_path = env::var("SEG_OUTPUT").expect("Segment output path is missing");
+
+//     let mut state = load_elf_with_patch(&elf_path, vec![]);
+//     // load input
+//     let args = env::var("ARGS").unwrap_or("data-to-hash".to_string());
+//     // assume the first arg is the hash output(which is a public input), and the second is the input.
+//     let args: Vec<&str> = args.split_whitespace().collect();
+//     assert_eq!(args.len(), 2);
+
+//     let public_input: Vec<u8> = hex::decode(args[0]).unwrap();
+//     state.add_input_stream(&public_input);
+//     log::info!("expected public value in hex: {:X?}", args[0]);
+//     log::info!("expected public value: {:X?}", public_input);
+
+//     let private_input = args[1].as_bytes().to_vec();
+//     log::info!("private input value: {:X?}", private_input);
+//     state.add_input_stream(&private_input);
+
+//     let (total_steps, mut state) = split_prog_into_segs(state, &seg_path, "", 0);
+
+//     let value = state.read_public_values::<[u8; 32]>();
+//     log::info!("public value: {:X?}", value);
+//     log::info!("public value: {} in hex", hex::encode(value));
+
+//     let seg_file = format!("{seg_path}/{}", 0);
+//     prove_single_seg_common(&seg_file, "", "", "", total_steps);
+// }
+
+// fn prove_revm() {
+//     // 1. split ELF into segs
+//     let elf_path = env::var("ELF_PATH").expect("ELF file is missing");
+//     let seg_path = env::var("SEG_OUTPUT").expect("Segment output path is missing");
+//     let json_path = env::var("JSON_PATH").expect("JSON file is missing");
+//     let seg_size = env::var("SEG_SIZE").unwrap_or("0".to_string());
+//     let seg_size = seg_size.parse::<_>().unwrap_or(0);
+//     let mut f = File::open(json_path).unwrap();
+//     let mut data = vec![];
+//     f.read_to_end(&mut data).unwrap();
+
+//     let mut state = load_elf_with_patch(&elf_path, vec![]);
+//     // load input
+//     state.add_input_stream(&data);
+
+//     let (total_steps, mut _state) = split_prog_into_segs(state, &seg_path, "", seg_size);
+
+//     let mut seg_num = 1usize;
+//     if seg_size != 0 {
+//         seg_num = (total_steps + seg_size - 1) / seg_size;
+//     }
+
+//     if seg_num == 1 {
+//         let seg_file = format!("{seg_path}/{}", 0);
+//         prove_single_seg_common(&seg_file, "", "", "", total_steps)
+//     } else {
+//         prove_multi_seg_common(&seg_path, "", "", "", seg_size, seg_num, 0).unwrap()
+//     }
+// }
+
+// #[derive(Debug, Clone, Deserialize, Serialize)]
+// pub enum DataId {
+//     TYPE1,
+//     TYPE2,
+//     TYPE3,
+// }
+
+// #[derive(Debug, Clone, Deserialize, Serialize)]
+// pub struct Data {
+//     pub input1: [u8; 10],
+//     pub input2: u8,
+//     pub input3: i8,
+//     pub input4: u16,
+//     pub input5: i16,
+//     pub input6: u32,
+//     pub input7: i32,
+//     pub input8: u64,
+//     pub input9: i64,
+//     pub input10: Vec<u8>,
+//     pub input11: DataId,
+//     pub input12: String,
+// }
+
+// impl Default for Data {
+//     fn default() -> Self {
+//         Self::new()
+//     }
+// }
+
+// impl Data {
+//     pub fn new() -> Self {
+//         let array = [1u8, 2u8, 3u8, 4u8, 5u8, 6u8, 7u8, 8u8, 9u8, 10u8];
+//         Self {
+//             input1: array,
+//             input2: 0x11u8,
+//             input3: -1i8,
+//             input4: 0x1122u16,
+//             input5: -1i16,
+//             input6: 0x112233u32,
+//             input7: -1i32,
+//             input8: 0x1122334455u64,
+//             input9: -1i64,
+//             input10: array[1..3].to_vec(),
+//             input11: DataId::TYPE3,
+//             input12: "hello".to_string(),
+//         }
+//     }
+// }
+
+// fn prove_add_example() {
+//     // 1. split ELF into segs
+//     let elf_path = env::var("ELF_PATH").expect("ELF file is missing");
+//     let seg_path = env::var("SEG_OUTPUT").expect("Segment output path is missing");
+//     let seg_size = env::var("SEG_SIZE").unwrap_or("0".to_string());
+//     let seg_size = seg_size.parse::<_>().unwrap_or(0);
+
+//     let mut state = load_elf_with_patch(&elf_path, vec![]);
+
+//     let data = Data::new();
+//     state.add_input_stream(&data);
+//     log::info!(
+//         "enum {} {} {}",
+//         DataId::TYPE1 as u8,
+//         DataId::TYPE2 as u8,
+//         DataId::TYPE3 as u8
+//     );
+//     log::info!("public input: {:X?}", data);
+
+//     let (total_steps, mut state) = split_prog_into_segs(state, &seg_path, "", seg_size);
+
+//     let value = state.read_public_values::<Data>();
+//     log::info!("public value: {:X?}", value);
+
+//     let mut seg_num = 1usize;
+//     if seg_size != 0 {
+//         seg_num = (total_steps + seg_size - 1) / seg_size;
+//     }
+
+//     if seg_num == 1 {
+//         let seg_file = format!("{seg_path}/{}", 0);
+//         prove_single_seg_common(&seg_file, "", "", "", total_steps)
+//     } else {
+//         prove_multi_seg_common(&seg_path, "", "", "", seg_size, seg_num, 0).unwrap()
+//     }
+// }
+
+// fn prove_host() {
+//     let host_program = env::var("HOST_PROGRAM").expect("host_program name is missing");
+//     match host_program.as_str() {
+//         "sha2_bench" => prove_sha2_bench(),
+//         "revm" => prove_revm(),
+//         "add_example" => prove_add_example(),
+//         _ => log::error!("Host program {} is not supported!", host_program),
+//     };
+// }
+
+// fn prove_segments() {
+//     let basedir = env::var("BASEDIR").unwrap_or("/tmp/cannon".to_string());
+//     let block = env::var("BLOCK_NO").unwrap_or("".to_string());
+//     let file = env::var("BLOCK_FILE").unwrap_or(String::from(""));
+//     let seg_dir = env::var("SEG_FILE_DIR").expect("segment file dir is missing");
+//     let seg_num = env::var("SEG_NUM").unwrap_or("1".to_string());
+//     let seg_num = seg_num.parse::<_>().unwrap_or(1usize);
+//     let seg_start_id = env::var("SEG_START_ID").unwrap_or("0".to_string());
+//     let seg_start_id = seg_start_id.parse::<_>().unwrap_or(0usize);
+//     let seg_size = env::var("SEG_SIZE").unwrap_or(format!("{SEGMENT_STEPS}"));
+//     let seg_size = seg_size.parse::<_>().unwrap_or(SEGMENT_STEPS);
+
+//     if seg_num == 1 {
+//         let seg_file = format!("{seg_dir}/{}", seg_start_id);
+//         prove_single_seg_common(&seg_file, &basedir, &block, &file, seg_size)
+//     } else {
+//         prove_multi_seg_common(
+//             &seg_dir,
+//             &basedir,
+//             &block,
+//             &file,
+//             seg_size,
+//             seg_num,
+//             seg_start_id,
+//         )
+//         .unwrap()
+//     }
+// }
+
+// // fn main() {
+// //     env_logger::try_init().unwrap_or_default();
+// //     let args: Vec<String> = env::args().collect();
+// //     let helper = || {
+// //         log::info!(
+// //             "Help: {} split | prove_segments | prove_host_program",
+// //             args[0]
+// //         );
+// //         std::process::exit(-1);
+// //     };
+// //     if args.len() < 2 {
+// //         helper();
+// //     }
+// //     match args[1].as_str() {
+// //         "split" => split_segments(),
+// //         "prove_segments" => prove_segments(),
+// //         "prove_host_program" => prove_host(),
+// //         _ => helper(),
+// //     };
+// // }
