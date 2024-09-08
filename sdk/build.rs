0115479441bc85de466104d4130f188e93ab2518
xander42280 <xander.z@zkm.io>
2024-09-08 12:20:17 +0800

    Option proto3_optional

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/proto/stage.proto")?;
+    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["src/proto/stage.proto"], &["src/proto"])?;
     Ok(())
 }
