d16b3f7ed0cbc843e3d16bf99f45d521b7394c6b
[gavin-ygy] <[gavin.ygy@gmail.com]>
2024-09-25 11:48:55 +0800

    SDK adds feature

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,8 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rustc-link-search=native=./sdk/src/local/libsnark");
-    println!("cargo:rustc-link-lib=dylib=snark");
-    tonic_build::configure()
-        .protoc_arg("--experimental_allow_proto3_optional")
-        .compile(&["src/proto/stage.proto"], &["src/proto"])?;
+    if let Some(_snark_flag) = std::env::var_os("NO_USE_SNARK") {
+        tonic_build::configure()
+            .protoc_arg("--experimental_allow_proto3_optional")
+            .compile(&["src/proto/stage.proto"], &["src/proto"])?;
+    } else {
+        println!("cargo:rustc-link-search=native=./sdk/src/local/libsnark");
+        println!("cargo:rustc-link-lib=dylib=snark");
+        tonic_build::configure()
+            .protoc_arg("--experimental_allow_proto3_optional")
+            .compile(&["src/proto/stage.proto"], &["src/proto"])?;
+    }
+
     Ok(())
 }
