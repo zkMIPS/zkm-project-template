33b0b51ed74f5c008f1c45a4b9d5da57015121d1
[gavin-ygy] <[gavin.ygy@gmail.com]>
2024-09-25 11:27:47 +0800

    check the write() results

--- host-program/src/bin/revme-prove.rs
+++ host-program/src/bin/revme-prove.rs
@@ -1,5 +1,5 @@
 use common::file;
-
+use std::fs;
 use std::env;
 use std::path::Path;
 use std::time::Instant;
@@ -33,20 +33,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //
     let start = Instant::now();
     let output_dir = env::var("OUTPUT_DIR").unwrap_or("/tmp/zkm".to_string());
+    match fs::create_dir_all(&output_dir) {
+        Ok(_) => log::info!("{} created successfully.", &output_dir),
+        Err(e) => {
+            log::info!("Failed to create directory {}, err: {}", &output_dir, e);
+            return Ok(());
+        }
+    }
     let proving_result = prover_client.prover.prove(&input, None).await;
     //match proverClient.await.prover.prover(&input,None).await {
     match proving_result {
         Ok(Some(prover_result)) => {
-            log::info!("Generating proof successfully .The proof file and verifier contract are in the path {}.",&output_dir);
             let output_path = Path::new(&output_dir);
-            let proof_result_path = output_path.join("snark_proof_with_public_inputs.json");
-            let _ = file::new(&proof_result_path.to_string_lossy())
-                .write(prover_result.proof_with_public_inputs.as_slice());
+            let mut f = file::new(&proof_result_path.to_string_lossy());
+            match  f.write(prover_result.proof_with_public_inputs.as_slice()) {
+                Ok(bytes_written) => {
+                    log::info!("Proof: successfully written {} bytes.", bytes_written);
+                },
+                Err(e) => {
+                    log::info!("Proof: failed to write to file: {}", e);
+                    return Ok(());
+                }
+            }
             //contract
             let output_path = Path::new(&output_dir);
             let contract_path = output_path.join("verifier.sol");
-            let _ = file::new(&contract_path.to_string_lossy())
-                .write(prover_result.solidity_verifier.as_slice());
+            let mut f = file::new(&contract_path.to_string_lossy());
+            match  f.write(prover_result.solidity_verifier.as_slice()){
+                Ok(bytes_written) => {
+                    log::info!("Contract: successfully written {} bytes.", bytes_written);
+                },
+                Err(e) => {
+                    log::info!("Contract: failed to write to file: {}", e);
+                    return Ok(());
+                }
+            }
+            log::info!("Generating proof successfully .The proof file and verifier contract are in the path {}.",&output_dir);
         }
         Ok(None) => {
             log::info!("Failed to generate proof.The result is None.");
