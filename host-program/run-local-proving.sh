3bf55fe2b2fa1829fbb2c3f275f2a58b9313a96a
eigmax <stephen@ieigen.com>
2024-10-07 09:55:07 +0800

    fix: compile guest automatically

--- host-program/run-local-proving.sh
+++ host-program/run-local-proving.sh
@@ -3,10 +3,15 @@ BASEDIR=$(cd $(dirname $0); pwd)
 export LD_LIBRARY_PATH=$BASEDIR/../sdk/src/local/libsnark:$LD_LIBRARY_PATH  ##Modify it according your template
 export ZKM_PROVER=local
 export RUST_LOG=info
-export SEG_SIZE=262144
+export SEG_SIZE=62144
 export ELF_PATH=${BASEDIR}/../guest-program/$program/target/mips-unknown-linux-musl/release/$program
 #export PUBLIC_INPUT_PATH=host-program/test-vectors/244.json
 #export OUTPUT_DIR=/tmp/$program
+
+(
+  cd ${BASEDIR}/../guest-program/$program
+  cargo build --target=mips-unknown-linux-musl --release
+)
 
 #nohup ../target/release/local-prove  >./local-proving.log 2>&1 &
 ../target/release/local-prove
