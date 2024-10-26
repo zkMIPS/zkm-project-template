4c5d0862482e41b2132a6eef80023c0836821bd1
[gavin-ygy] <[gavin.ygy@gmail.com]>
2024-10-26 17:19:11 +0800

    update contract

--- contracts/src/verifier.sol
+++ contracts/src/verifier.sol
@@ -4,7 +4,7 @@
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-import {console}  from "forge-std/console.sol";
+
 pragma solidity ^0.8.0;
 library Pairing {
     struct G1Point {
@@ -225,7 +225,7 @@ contract Verifier {
         uint32[8] memory _memRootAfter
     ) public pure returns (uint256) {
         bytes32 userData = sha256(_userData);
-console.log("********* userData 11111111");
+
         uint256 memRootBefore = 0;
         for (uint256 i = 0; i < 8; i++) {
             memRootBefore |= uint256(_memRootBefore[i]) << (32 * (7 - i));
