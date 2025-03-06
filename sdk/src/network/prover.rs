637806d78a72c069ff5ed6844f1ffa362f61146b
eigmax <stephen@ieigen.com>
2025-03-06 10:29:05 +0800

    feat: remove zkm prover

--- sdk/src/network/prover.rs
+++ sdk/src/network/prover.rs
@@ -1,9 +1,10 @@
-use common::tls::Config;
 use stage_service::stage_service_client::StageServiceClient;
 use stage_service::{GenerateProofRequest, GetStatusRequest};
 
+use std::path::Path;
 use std::time::Instant;
 use tonic::transport::Endpoint;
+use tonic::transport::{Certificate, Identity};
 use tonic::transport::{Channel, ClientTlsConfig};
 
 use crate::prover::{ClientCfg, Prover, ProverInput, ProverResult};
@@ -12,6 +13,12 @@ use tokio::time::sleep;
 use tokio::time::Duration;
 
 use async_trait::async_trait;
+
+#[derive(Clone)]
+pub struct Config {
+    pub ca_cert: Option<Certificate>,
+    pub identity: Option<Identity>,
+}
 
 pub mod stage_service {
     tonic::include_proto!("stage.v1");
@@ -32,7 +39,9 @@ impl NetworkProver {
         let ssl_config = if ca_cert_path.is_empty() {
             None
         } else {
-            Some(Config::new(ca_cert_path, cert_path, key_path).await?)
+            let (ca_cert, identity) =
+                get_cert_and_identity(ca_cert_path, cert_path, key_path).await?;
+            Some(Config { ca_cert, identity })
         };
         let endpoint_para = client_config.endpoint.to_owned().expect("ENDPOINT must be set");
         let endpoint = match ssl_config {
@@ -216,4 +225,36 @@ impl Prover for NetworkProver {
         log::info!("calling wait_proof, proof_id={}", proof_id);
         self.wait_proof(&proof_id, timeout).await
     }
+}
+
+async fn get_cert_and_identity(
+    ca_cert_path: String,
+    cert_path: String,
+    key_path: String,
+) -> anyhow::Result<(Option<Certificate>, Option<Identity>)> {
+    let ca_cert_path = Path::new(&ca_cert_path);
+    let cert_path = Path::new(&cert_path);
+    let key_path = Path::new(&key_path);
+    // if !ca_cert_path.is_file() || !cert_path.is_file() || !key_path.is_file() {
+    //     bail!("both ca_cert_path, cert_path and key_path should be valid file")
+    // }
+    let mut ca: Option<Certificate> = None;
+    let mut identity: Option<Identity> = None;
+    if ca_cert_path.is_file() {
+        let ca_cert = tokio::fs::read(ca_cert_path)
+            .await
+            .unwrap_or_else(|err| panic!("Failed to read {:?}, err: {:?}", ca_cert_path, err));
+        ca = Some(Certificate::from_pem(ca_cert));
+    }
+
+    if cert_path.is_file() && key_path.is_file() {
+        let cert = tokio::fs::read(cert_path)
+            .await
+            .unwrap_or_else(|err| panic!("Failed to read {:?}, err: {:?}", cert_path, err));
+        let key = tokio::fs::read(key_path)
+            .await
+            .unwrap_or_else(|err| panic!("Failed to read {:?}, err: {:?}", key_path, err));
+        identity = Some(Identity::from_pem(cert, key));
+    }
+    Ok((ca, identity))
 }
